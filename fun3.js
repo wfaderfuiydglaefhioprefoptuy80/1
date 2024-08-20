cbb21babb3b7ea47072106e6ab09634b025ebfa0
wfaderfuiydglaefhioprefoptuy80 <auttpoperationamalgamation29@gmail.com>
2024-08-19 23:17:42 -0400

    Delete fun3.js

--- fun3.js
+++
@@ -1,6 +0,0 @@
-const payload = `<img src="x" onerror="fetch('https://cakey.bot/assets/', {mode: 'no-cors'})
-  .then(response => response.text())
-  .then(data => {
-    console.log(data);
-  })
-document.body.innerHTML += payload; // Inject the payload into the body
