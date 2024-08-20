9669803fc2c74cf607c426b4090d01b5ed50d414
wfaderfuiydglaefhioprefoptuy80 <auttpoperationamalgamation29@gmail.com>
2024-08-19 23:30:31 -0400

    Delete fun4.js

--- fun4.js
+++
@@ -1,8 +0,0 @@
-const payload = `<img src="x" onerror="fetch('https://cakey.bot/assets/', {mode: 'no-cors'})
-  .then(response => response.text())
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => console.error('Error:', error));">`;
-
-document.body.innerHTML += payload;
